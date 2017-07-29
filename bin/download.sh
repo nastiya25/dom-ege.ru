6a197b4ac0f7a59fcb80cae99863d60e60fd1514
Alesenko Elena <alesenko.e@drofa.ru>
2017-07-29 22:30:02 +0300

    обновляем файлы

--- bin/download.sh
+++ bin/download.sh
@@ -6,3 +6,7 @@ set -o pipefail
 ./node_modules/.bin/gsjson 11HIARllbIOPq4FgpZbrchu80XvjRgLtVsfIbGFgP6s0 |
 	./node_modules/.bin/index-by menu |
 	./node_modules/.bin/json > _data/teachers.json
+  
+  
+curl 'https://docs.google.com/spreadsheets/d/11HIARllbIOPq4FgpZbrchu80XvjRgLtVsfIbGFgP6s0/pub?gid=368780656&single=true&output=csv' > _data/subjects.csv
+curl 'https://docs.google.com/spreadsheets/d/11HIARllbIOPq4FgpZbrchu80XvjRgLtVsfIbGFgP6s0/pub?gid=217644199&single=true&output=csv' > _data/reviews.csv
