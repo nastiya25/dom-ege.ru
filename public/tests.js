ca48c69be87404bb7a53c78ea4215c4b2d4b8163
Aleksej Kamynin <zendzirou@gmail.com>
2017-07-29 13:34:18 +0300

    Переписал по человечьи

--- public/tests.js
+++ public/tests.js
@@ -1,10 +1,38 @@
-
-setTimeout(function(){$('.hide-block').fadeOut('fast')},60000*{{ page.testtime }});  //30000 = 30 секунд
-setTimeout(function(){$('.box-show').css("display", "block")},60000*{{ page.testtime }});  //30000 = 30 секунд
-function timery(){
-  var obj=document.getElementById('timer');
-  obj.innerHTML--;
-  if(obj.innerHTML==0){setTimeout(function(){},60000*{{ page.testtime }});}
-  else{setTimeout(timery,60000*{{ page.testtime }});}
+function declint(number, titles){
+  return titles[(number%100>4 && number%100<20) ?  2 : ([2, 0, 1, 1, 1, 2])[Math.min(number%10, 5)]].replace('%s', number)
 }
-setTimeout(timery,60000*{{ page.testtime }});
+function initTest(minutes){
+  var minutesLeft = minutes;
+  var timeElement;
+
+  function hideTest(){
+    $('.hide-block').fadeOut('fast');
+  }
+
+  function showFinishMessage(){
+    $('.box-show').css("display", "block");
+  }
+
+  function setupLimit(){
+    setTimeout(function(){
+      hideTest();
+      showFinishMessage();
+    }, 60000 * minutes);
+  }
+
+  function renderMinutes(){
+    timeElement.html(declint(minutesLeft, ['%s минута', '%s минуты', '%s минут']));
+  }
+  function startTimer(){
+    renderMinutes()
+    setInterval(function(){
+      minutesLeft--;
+      renderMinutes()
+    }, 60000 * minutes);
+  }
+  $(function(){
+    timeElement = $('#timer');
+    setupLimit();
+    startTimer();
+  });
+}
